60c39d1ff50b4fc105e2cd1cfaaa20317ac81529
Nikita Volobuev <nikitavbv@gmail.com>
2021-01-05 21:49:14 +0200

    feat(frontend): add types for namespaces api

---
+++ frontend/src/types/namespaces.ts
@@ -0,0 +1,21 @@
+export type Namespace = {
+    name: string,
+    pods: Pod[],
+};
+
+export type Pod = {
+    name: string,
+    containers: Container[],
+};
+
+export type Container = {
+    name: string,
+    usage: Resources | null,
+    requests: Resources | null,
+    limits: Resources | null,
+};
+
+export type Resources = {
+    cpu: number, // nanocpus
+    memory: number, // kilobytes
+};
