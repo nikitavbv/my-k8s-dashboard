99f37a03606ccc5683549c5e98e1d709bc2fbc69
Nikita Volobuev <nikitavbv@gmail.com>
2021-01-10 19:30:58 +0200

    feat(frontend): simple collapse for namespaces

--- frontend/src/components/NamespaceRow.tsx
+++ frontend/src/components/NamespaceRow.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Namespace } from '../types';
 import { PodRow } from './PodRow';
+import {ChevronRight} from './icons';
 
 export type NamespaceRowProps = {
     namespace: Namespace,
 };
 
 export const NamespaceRow = (props: NamespaceRowProps) => {
+    const [collapsed, setIsCollapsed] = useState<Boolean>(true);
+
     return (
-        <div>
-            - Namespace: { props.namespace.name }<br />
-            { props.namespace.pods.map(pod => (<PodRow pod={pod} />) )}
+        <div style={{
+            borderTop: '1px solid rgb(224, 224, 224)',
+            padding: '8px',
+            userSelect: 'none',
+        }}>
+            <ChevronRight onClick={() => setIsCollapsed(!collapsed)}  style={{
+                display: 'inline',
+                cursor: 'pointer',
+                lineHeight: '20px',
+            }}/>
+            <span style={{ lineHeight: '20px' }}>
+                Namespace: { props.namespace.name }
+            </span>
+            <div style={{
+                display: collapsed ? 'none' : 'inherit',
+            }}>
+                { props.namespace.pods.map(pod => (<PodRow pod={pod} />) )}
+            </div>
         </div>
     );
 };
---
+++ frontend/src/components/icons/types.ts
@@ -0,0 +1,6 @@
+import {CSSProperties} from 'react';
+
+export type IconProps = {
+    style: CSSProperties,
+    onClick: () => void,
+};
