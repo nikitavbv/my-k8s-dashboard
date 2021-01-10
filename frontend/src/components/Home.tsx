2c57408dce5b0b0739fe8137c30fa4e4bd9917d8
Nikita Volobuev <nikitavbv@gmail.com>
2021-01-10 17:49:44 +0200

    feat(frontend): list namespaces

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const Home = () => (
-    <div>Home</div>
-);
+import { Namespace } from '../types';
+
+type NamespacesResponse = {
+    namespaces: Namespace[],
+};
+
+export const Home = () => {
+    const [namespaces, setNamespaces] = useState<Namespace[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/namespaces')
+            .then(r => r.json() as unknown as NamespacesResponse)
+            .then(r => setNamespaces(r.namespaces));
+    }, []);
+
+    console.log('namespaces are', namespaces);    
+
+    return (
+        <React.Fragment>
+            <div>Home</div>
+            { namespaces.map(namespace => (<div>namespace: {namespace.name}</div>)) }
+        </React.Fragment>
+    );
+};
