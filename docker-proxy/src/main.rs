aff7465f134a8527a6e0704fa4d50e5c0f89f72d
Nikita Volobuev <nikitavbv@gmail.com>
2021-08-20 09:42:52 +0300

    feat(docker-proxy): log more events

--- docker-proxy/src/main.rs
+++ docker-proxy/src/main.rs
@@ -19,8 +19,9 @@ async fn main() {
     let client_key = thrussh_keys::key::KeyPair::generate_ed25519().unwrap();
     let client_pubkey = Arc::new(client_key.clone_public_key());
     let mut config = thrussh::server::Config::default();
-    config.connection_timeout = Some(std::time::Duration::from_secs(3));
-    config.auth_rejection_time = std::time::Duration::from_secs(3);
+    // For some reason it takes connection_timeout for client to connect
+    config.connection_timeout = Some(std::time::Duration::from_secs(10));
+    config.auth_rejection_time = std::time::Duration::from_secs(30);
     config.keys.push(thrussh_keys::key::KeyPair::generate_ed25519().unwrap());
     let config = Arc::new(config);
     let sh = Server {
@@ -71,6 +72,11 @@ impl server::Handler for Server {
         futures::future::ready(Ok((self, session)))
     }
 
+    fn channel_open_direct_tcpip(self, channel: ChannelId, host_to_connect: &str, port_to_connect: u32, originator_address: &str, originator_port: u32, session: Session) -> Self::FutureUnit {
+        info!("opening direct tcpip");
+        futures::future::ready(Ok((self, session)))
+    }
+
     fn channel_open_session(self, channel: ChannelId, session: server::Session) -> Self::FutureUnit {
         info!("openning session to a new client");
         
@@ -102,5 +108,30 @@ impl server::Handler for Server {
 
         futures::future::ready(Ok((self, session)))
         //self.finished(session)
+    }
+
+    fn extended_data(self, channel: ChannelId, code: u32, data: &[u8], session: Session) -> Self::FutureUnit {
+        info!("got extended data");
+        futures::future::ready(Ok((self, session)))
+    }
+
+    fn pty_request(self, channel: ChannelId, term: &str, col_width: u32, row_height: u32, pix_width: u32, pix_height: u32, modes: &[(thrussh::Pty, u32)], session: Session) -> Self::FutureUnit {
+        info!("got pty request");
+        futures::future::ready(Ok((self, session)))
+    }
+
+    fn shell_request(self, channel: ChannelId, session: Session) -> Self::FutureUnit {
+        info!("got shell request");
+        futures::future::ready(Ok((self, session)))
+    }
+
+    fn exec_request(self, channel: ChannelId, data: &[u8], session: Session) -> Self::FutureUnit {
+        info!("got exec request: {}", String::from_utf8_lossy(data));
+        futures::future::ready(Ok((self, session)))
+    }
+
+    fn tcpip_forward(self, address: &str, port: u32, session: Session) -> Self::FutureBool {
+        info!("got tcpip forward");
+        futures::future::ready(Ok((self, session, true)))
     }
 }
